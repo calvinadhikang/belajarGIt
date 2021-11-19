3e808a3b09b180dae8af52db8554b99cdc2dccdc
Calvin Adhikang <66165193+calvinadhikang@users.noreply.github.com>
2021-11-20 00:41:56 +0700

    Add files via upload

---
+++ src/iniapps1/User.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iniapps1;
+
+/**
+ *
+ * @author calvi
+ */
+public class User {
+    String nama, umur;
+    
+    public User (String nama, String umur){
+        this.nama = nama;
+        this.umur = umur;
+    }
+}
